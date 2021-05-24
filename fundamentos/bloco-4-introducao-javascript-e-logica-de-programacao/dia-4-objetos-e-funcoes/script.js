3139095c78c51824ae029c1bbc1acb606488671d
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-05-24 19:22:02 -0300

    Resolve parte 1 do dia 4 do bloco 4

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/script.js
@@ -0,0 +1,47 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+// // console.log(`Bem-vinda, ${info.personagem}`) 
+// Resolve exercicio 2.
+
+
+// for (let key in info) {
+//   console.log(key);
+// }
+// Resolve exercicio 3.
+
+
+
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+// Resolve exercicio 4.
+
+
+
+
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//   nota: 'O último MacPatinhas',
+//   recorrente: 'Sim',
+// };
+
+// for (let key in info) {
+//   if (
+//     info[key] === info.recorrente &&
+//     info[key] === 'Sim' &&
+//     info2[key] === 'Sim'
+//   ) {
+//     console.log('Ambos recorrentes');
+//   } else {
+//     console.log(info[key] + ' e ' + info2[key]);
+//   }
+// }
+// Resolve exercicio 5.
+
+
