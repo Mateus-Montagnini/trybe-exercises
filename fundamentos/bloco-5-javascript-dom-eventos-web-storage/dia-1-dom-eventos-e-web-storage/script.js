77a8a6d33fb8a7bc68b92ed56e827fc7b8e11e34
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-05-26 20:22:10 -0300

    Resolve exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-1-dom-eventos-e-web-storage/script.js
@@ -0,0 +1,5 @@
+function changeText() {
+  let paragraph = document.getElementsByTagName('p')[1];
+  paragraph.innerHTML = 'Estar formado em Engenharia e trabalhando com programcao'
+}
+changeText();
