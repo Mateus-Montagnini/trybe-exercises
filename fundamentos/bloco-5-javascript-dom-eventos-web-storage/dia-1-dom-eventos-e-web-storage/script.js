1acfb167ad4f3bfd50b5917d4736c3c9c3b490fb
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-05-26 21:02:32 -0300

    Resolve exercicio 6

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-1-dom-eventos-e-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-1-dom-eventos-e-web-storage/script.js
@@ -27,3 +27,11 @@ function paragraphUpperCase() {
   paragraph.innerHTML = paragraph.innerHTML.toUpperCase();
 }
 paragraphUpperCase();
+
+function showAllParagraphs() {
+  let paragraphs = document.getElementsByTagName('p');
+      for (let index = 0; index < paragraphs.length; index += 1) {
+        console.log(paragraphs[index].innerHTML);
+      }
+    }
+    showParagraphs();
